5c1305b49d81921f657033485cedf74d7512b59c
onionmccabbage <onionmccabbage@live.com>
2025-08-04 12:44:18 +0100

    interactive content

--- main.js
+++ main.js
@@ -19,4 +19,12 @@ const handleButton = ()=>{
     // prevent the form from refreshing the page!!!!!!!
     event.preventDefault()
     console.log(`${event.target}`)
+    // grab the form field
+    let stock = document.getElementById('stockCode')
+    let stockCodeChoice = stock.value // value is the current value in the form field
+    let shop = document.getElementById('shopping')
+    // we can build a little piece of HTML
+    let c = document.createElement('p')
+    c.append(stockCodeChoice) // append the litteral content
+    shop.appendChild(c) // append as HTML content
 }
