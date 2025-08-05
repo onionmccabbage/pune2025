c93f59890412d29cc2f6480b0d1ccd8bd87ebd9d
onionmccabbage <onionmccabbage@live.com>
2025-08-05 10:02:24 +0100

    using fetch

--- modular/paymentAPIaccess.js
+++ modular/paymentAPIaccess.js
@@ -1,14 +1,28 @@
 // access data from an API end-point
 // we will use promise where we must wait for the data
 
-const getAllAPIPayments = () => {
+// we use async/await to let our codee handle time-dependant outcomes (promises)
+
+const getAllAPIPayments = async () => {
     const url = "https://payments.neueda.com/api/payment"
-    // by default fecth will use 'GET'
-    // We may coose ot specify the HTTP verb GET, POST, PUT, UPDATE, PATCH
-    const response = fetch(url, { 
+    // by default fetch will use 'GET'
+    // We may coose to specify the HTTP verb GET, POST, PUT, UPDATE, PATCH
+    try {
+        const response = await fetch(url, { 
             method: 'GET', 
             headers: new Headers(
             { 'Accept': 'application/json' }) 
             }
         )
+        // check to see if the response is ok
+        if (!response.ok){
+            throw new Error(`Something went wrong ${response.status}`)
+        }
+        // if we get this far we know the response is ok
+        console.log(response)
+    }
+    catch (err) {
+        console.error(err.message)
+    }
+
 }
