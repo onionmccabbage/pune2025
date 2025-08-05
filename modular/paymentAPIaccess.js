37789f643a6151d1e37250b463b546556a777287
onionmccabbage <onionmccabbage@live.com>
2025-08-05 10:06:51 +0100

    using fetch

--- modular/paymentAPIaccess.js
+++ modular/paymentAPIaccess.js
@@ -20,9 +20,15 @@ const getAllAPIPayments = async () => {
         }
         // if we get this far we know the response is ok
         console.log(response)
+        return response
     }
     catch (err) {
         console.error(err.message)
     }
-
 }
+//export this function as the default for this module
+export default getAllAPIPayments
+
+
+// remember to call the function (for exercising the code within this module - NOT for export)
+getAllAPIPayments()
