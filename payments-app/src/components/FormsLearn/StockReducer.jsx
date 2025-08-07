8ac16f8b20fd2ec8edc18d2c3e33a1fff7e009bd
onionmccabbage <onionmccabbage@live.com>
2025-08-07 07:40:29 +0100

    reducer

---
+++ payments-app/src/components/FormsLearn/StockReducer.jsx
@@ -0,0 +1,32 @@
+
+
+const StockformReducer = ()=>{
+    // component logic
+
+
+    // UI
+    return (
+        // CAREFUL - all components must have ONE root tag
+        <section>
+            <form>
+                {/* in React form fields are 'controlled' */}
+                <input id="newStockCode" placeholder="Stock Code"
+                    onChange={changeStockcode} />
+                {/* add a field for 'stockPrice' controlled by a stateful model 'stockPrice */}
+                <input type="number" id='newStockPrice' placeholder="Stock Price" 
+                    min="0" onChange={changeStockPrice}/>
+                {/* User Experience Principles: give feedback */}
+                <input type="range" id="newStockQuantity" value={stockQty}
+                    min={qtyMin} max={qtyMax} onChange={handleQuantity} /><span>{stockQty}</span>
+                <button>Go </button>
+            </form>
+            <aside>
+                {/* we can monitor any stateful values */}
+                <p>Stock Code: {stockCode} Price:{stockPrice} Quantity {stockQty}</p>
+            </aside>
+        </section>
+    )
+}
+
+
+export default StockformReducer
