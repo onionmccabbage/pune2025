5c5c6aa97e938eb73b50c5fd9bb7146fbccf55f2
onionmccabbage <onionmccabbage@live.com>
2025-08-07 06:53:03 +0100

    React controlled form fields

--- payments-app/src/components/FormsLearn/StockForm.jsx
+++ payments-app/src/components/FormsLearn/StockForm.jsx
@@ -2,10 +2,14 @@ import { useState } from "react"
 
 const StockForm = ()=>{
     // component logic
+    const qtyMin = 1
+    const qtyMax = 100
     // whenever you see 'use' that is a React hook
     const [stockCode, setStockCode] = useState()
     // state for stockPrice
     const [stockPrice, setStockPrice] = useState()
+    // state for quantity with an initial state of 1
+    const [stockQty, setStockQty] = useState(1)
 
     // handlers
     const changeStockcode = (e)=>{
@@ -19,7 +23,10 @@ const StockForm = ()=>{
         const price =e.target.value
         setStockPrice(price)
     }
-
+    const handleQuantity = (e)=>{
+        const qty = e.target.value
+        setStockQty(qty)
+    }
     // component content (what the user sees)
     return (
         // CAREFUL - all components must have ONE root tag
@@ -29,15 +36,16 @@ const StockForm = ()=>{
                 <input id="newStockCode" placeholder="Stock Code"
                     onChange={changeStockcode} />
                 {/* add a field for 'stockPrice' controlled by a stateful model 'stockPrice */}
-                <input id='newStockPrice' placeholder="Stock Price" 
-                    onChange={changeStockPrice}/>
-                <button>
-                    Go
-                </button>
+                <input type="number" id='newStockPrice' placeholder="Stock Price" 
+                    min="0" onChange={changeStockPrice}/>
+                {/* User Experience Principles: give feedback */}
+                <input type="range" id="newStockQuantity" value={stockQty}
+                    min={qtyMin} max={qtyMax} onChange={handleQuantity} /><span>{stockQty}</span>
+                <button>Go </button>
             </form>
             <aside>
                 {/* we can monitor any stateful values */}
-                <p>Stock Code: {stockCode} Price:{stockPrice}</p>
+                <p>Stock Code: {stockCode} Price:{stockPrice} Quantity {stockQty}</p>
             </aside>
         </section>
     )
